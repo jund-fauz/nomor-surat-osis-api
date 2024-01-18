7767d87e276aaddc8dc09db1fc246722e12d87d5
jund-fauz <mujadidjundifauzi200@gmail.com>
2024-01-18 13:32:55 +0700

    feat: change link data

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ app.get('/nomor-surat', (req, res) => {
 
 app.post('/nomor-surat', (req, res) => {
     const date = new Date()
-    const { jenisSurat, pengirim, perihal = null, link = null } = req.body
-    const query = `INSERT INTO ${table1} (jenis_surat, pengirim, tanggal, bulan, tahun, perihal, link) VALUES ('${jenisSurat}', '${pengirim}', ${date.getDate()}, ${date.getMonth()+1}, ${date.getFullYear()}, '${perihal}', '${link}')`
+    const { jenisSurat, pengirim, tanggal = date.getDate(), bulan = date.getMonth()+1, tahun = date.getFullYear(), perihal = null, link = null } = req.body
+    const query = `INSERT INTO ${table1} (jenis_surat, pengirim, tanggal, bulan, tahun, perihal, link) VALUES ('${jenisSurat}', '${pengirim}', ${tanggal}, ${bulan}, ${tahun}, '${perihal}', '${link}')`
     db.query(query, (error, fields) => {
         if (error) response(res, 'invalid', 'error', 500)
         if (fields?.affectedRows) {
@@ -45,6 +45,24 @@ app.put('/nomor-surat/:id', (req, res) => {
     const { id } = req.params
     const { jenisSurat, pengirim, perihal = null, link = null } = req.body
     const query = `UPDATE ${table1} SET jenis_surat = '${jenisSurat}', pengirim = '${pengirim}', perihal = '${perihal}', link = '${link}' WHERE id = ${id}`
+    db.query(query, (error, fields) => {
+        if (error) response(res, 'invalid', 'error', 500)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                id: fields.insertId
+            }
+            response(res, 'Data Updated Successfuly', data, 200)
+        } else {
+            response(res, 'Data tidak ditemukan', 'error: not found', 404)
+        }
+    })
+})
+
+app.put('/nomor-surat/:id/link', (req, res) => {
+    const { id } = req.params
+    const { link } = req.body
+    const query = `UPDATE ${table1} SET link = '${link}' WHERE id = ${id}`
     db.query(query, (error, fields) => {
         if (error) response(res, 'invalid', 'error', 500)
         if (fields?.affectedRows) {
