001e81174c385d076176e4a9f64f6648c5ec720c
jund-fauz <mujadidjundifauzi200@gmail.com>
2024-01-19 14:41:16 +0700

    Add: CORS origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const table1 = '`nomor-osis`'
 const table2 = '`periode-osis`'
 
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({ origin: 'https://jund-fauz.github.io'}))
 
 require('dotenv').config()
 
