b73c1ea8b2b634d56c868db9526e0af6a115ed25
jund-fauz <mujadidjundifauzi200@gmail.com>
2024-02-17 17:59:17 +0700

    Still Try to Fix: CORS Issue

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const table2 = '`periode-osis`'
 app.use(bodyParser.json())
 app.use(cors())
 
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+})
+
 require('dotenv').config()
 
 app.get('/', (req, res) => {
