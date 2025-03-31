86b6ebd79a62c76f931216db40cc1bb47dc3a344
jund.fauz <mujadidjundifauzi200@gmail.com>
2025-03-31 16:00:53 +0700

    update: migrate to PostgreSQL

--- config/database.js
+++ config/database.js
@@ -5,8 +5,14 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_DB, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
     host: process.env.DATABASE_HOSTNAME,
-    dialect: "mysql",
-    logging: false, 
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 export default db;
