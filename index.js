8095f7411fc3be8f380612a9f4cb3fd4a079f576
asus <rasyadelgiva@gmail.com>
2025-07-11 14:56:14 +0700

    fix all error

--- index.js
+++ index.js
@@ -11,12 +11,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(router);
 
 // Serve static files from public directory
 app.use(express.static('public'));
-
-app.use(router);
-
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { getNomorSurat, createNomorSurat, updateNomorSurat, deleteNomorSurat, searchNomorSurat } from "../controllers/nomorSuratController.js";
-import { getAkun, register, login, forgotPassword } from "../controllers/AkunController.js";
 import { auth } from "./middleware/auth.js";
+import { getNomorSurat, createNomorSurat, updateNomorSurat, deleteNomorSurat, searchNomorSurat } from "../controllers/NomorSuratController.js";
+import { getAkun, register, login, forgotPassword } from "../controllers/AkunController.js";
 import { createOrganisasi, getOrganisasi } from "../controllers/OrganisasiController.js";
 
 const router = express.Router();
@@ -11,13 +11,6 @@ router.get("/", (req, res) => {
   res.send("API Berhasil!");
 })
 
-//nomor surat
-router.get("/nomor-surat",auth, getNomorSurat);
-router.post("/nomor-surat",auth, createNomorSurat);
-router.put("/nomor-surat/:id",auth, updateNomorSurat);
-router.delete("/nomor-surat/:id", auth, deleteNomorSurat);
-router.post("/nomor-surat/search", auth, searchNomorSurat)
-
 //akun
 router.get("/akun", getAkun);
 router.post("/akun", register);
@@ -25,5 +18,13 @@ router.post("/login", login);
 router.get("/organisasi", getOrganisasi);
 router.put("/forgot-password", forgotPassword);
 router.post("/organisasi", createOrganisasi)
+
+//nomor surat
+router.get("/nomor-surat",auth, getNomorSurat);
+router.post("/nomor-surat",auth, createNomorSurat);
+router.put("/nomor-surat/:id",auth, updateNomorSurat);
+router.delete("/nomor-surat/:id", auth, deleteNomorSurat);
+router.post("/nomor-surat/search", auth, searchNomorSurat)
+
 
 export default router;
